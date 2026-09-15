0864f9ce5531f069b3335f3b99d176a6f93d9663
safi-ahmed43 <safiahmed1243@gmail.com>
2026-09-15 19:55:55 +0600

    added interface code

---
+++ Module 4/Interface/interface.dart
@@ -0,0 +1,37 @@
+import '../Abstraction/practice.dart';
+
+abstract class Shape{
+  void calculateArea();
+}
+
+class CircleArea implements Shape{
+  double radius;
+  CircleArea(this.radius);
+
+  @override
+  void calculateArea() {
+    double area = 3.1416 * radius*radius;
+    print('Circle Area is:$area');
+  }
+}
+
+class SquareArea implements Shape{
+  double value;
+  SquareArea(this.value);
+
+  @override
+  void calculateArea() {
+    double area = value * value;
+    print('Square Area is: $area');
+  }
+}
+
+void main(){
+  print('\n--------------Circle Area -----------');
+  CircleArea circle = CircleArea(5.0);
+  circle.calculateArea();
+  print('-------Square Area----------');
+  SquareArea square = SquareArea(12);
+  square.calculateArea();
+
+}
